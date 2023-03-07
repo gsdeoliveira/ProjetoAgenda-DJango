fed97adb42fd1606185e9c66a0a1111690b4f512
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-03-07 08:57:27 -0300

    configurações da área administrativa

--- contatos/models.py
+++ contatos/models.py
@@ -6,6 +6,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
@@ -14,3 +17,6 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.nome
