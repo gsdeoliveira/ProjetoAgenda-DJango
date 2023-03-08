7dbf4efd9b1a7d78804eab7b018ad94b0b0bc5e9
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-03-08 14:05:45 -0300

    erros 404 e condicionais

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,11 +4,12 @@ from . models import Categoria, Contato
 # Register your models here.
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     #  list_filter = ('nome', 'sobrenome')
     list_per_page = 10
     search_fields = ('nome', 'sobrenome', 'telefone')
+    list_editable = ('telefone', 'mostrar')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
