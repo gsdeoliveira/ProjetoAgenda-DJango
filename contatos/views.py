73d78f93975745e62a87d263bb2ff32d356e479a
gsdeoliveira <gabriielsousa2002@gmail.com>
2023-03-09 12:58:35 -0300

    campo de pesquisa

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def index(request):
     contatos = Contato.objects.order_by('-id').filter(
@@ -32,3 +34,32 @@ def ver_contato(request, contato_id):
     #  except Contato.DoesNotExist as e:
     #  raise Http404()
 
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None:
+        raise Http404
+    
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    """
+    contatos = Contato.objects.order_by('-id').filter(
+        Q(nome__icontains=termo) | Q(sobrenome__icontains=termo), # Consulta com OR
+        mostrar=True
+    )
+    """
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 20)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
+    })
